746fbb892dff4960c04bf9e6da3a0c2123d3b9b6
DrSpacemanMD <Johnt717@gmail.com>
2025-09-11 17:56:26 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -6,5 +6,5 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read()
 
 st.write(df.head())
-fig = px.scatter(df, x=df.columns[0], y=df.columns[1], title="Scatter Plot")
-st.plotly_chart(fig)
+#fig = px.scatter(df, x=df.columns[0], y=df.columns[1], title="Scatter Plot")
+#st.plotly_chart(fig)
