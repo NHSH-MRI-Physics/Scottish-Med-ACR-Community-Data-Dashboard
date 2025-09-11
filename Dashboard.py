1110cfaa3d9e9dcdceaec56b2323a048e8407c4e
DrSpacemanMD <Johnt717@gmail.com>
2025-09-11 17:55:51 +0100

    getting things ready for streamlit

--- Dashboard.py
+++ Dashboard.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import plotly.express as px
 
-st.title("Hello, World!")
-st.write("Welcome to your first Streamlit page.")
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read()
+
+st.write(df.head())
+fig = px.scatter(df, x=df.columns[0], y=df.columns[1], title="Scatter Plot")
+st.plotly_chart(fig)
